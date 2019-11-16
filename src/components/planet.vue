e04f52bd586c74c03edf13acce0168ebc7f28ed3
saierti <310320717@qq.com>
2019-11-17 01:25:43 +0800

    搜索问题

--- src/components/planet.vue
+++ src/components/planet.vue
@@ -2,7 +2,7 @@
   <div class="planet">
       <div class="background"></div>
       <div class="search">
-          <form action="#">
+          <form action="#" @submit="()=>{return false}">
               <input type="text" class="search-bar" placeholder="点击搜索" @focus="changePage(true)" v-model="searchText" @keypress="search($event)">
           </form>
       </div>
@@ -195,7 +195,6 @@ export default {
                 }).catch(err=>{
                     console.log("搜索失败")
                 })
-                // return false;
             }
         },
         toTopic(item,event){
